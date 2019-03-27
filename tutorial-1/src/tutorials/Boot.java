5ee3c27034be77c28af4066ec9c25b228c4f7799
CoffeeBeanCode <48706059+CoffeeBeanCode@users.noreply.github.com>
2019-03-26 20:44:04 -0400

    Update Boot.java

--- tutorial-1/src/tutorials/Boot.java
+++ tutorial-1/src/tutorials/Boot.java
@@ -1,49 +1,99 @@
 package tutorials;
 
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.LWJGLException;
+import java.nio.IntBuffer;
 
+import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
 
-import tutorials.render.Mesh;
-import tutorials.render.MeshLoader;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Boot {
 
-  public Boot(){
-    try{
-      Display.setDisplayMode(new DisplayMode(640,480));
-      Display.setTitle("OpenGL Bootcamp");
-      Display.create();
-    }catch(LWJGLException e){
-      e.printStackTrace();
+  private long window;
+
+  public void run(){
+    init();
+    loop();
+    
+    Callbacks.glfwFreeCallbacks(window);
+    GLFW.glfwDestroyWindow(window);
+		
+    GLFW.glfwTerminate();
+    GLFW.glfwSetErrorCallback(null).free();
+  }
+  
+  /*
+  *Initialize GLFW and create the window
+  */
+  public void init(){
+    GLFWErrorCallback.createPrint(System.err).set();
+		
+    if(!GLFW.glfwInit()) {
+	  	throw new IllegalStateException("Unable to initialize GLFW");
     }
-    
-    float[] vertices = {-0.5f,-0.5f, 0f,
-                         0.5f,-0.5f, 0f,
-                         0.0f, 0.5f, 0f};
+		
+    GLFW.glfwDefaultWindowHints();
+    GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+    GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+		
+    window = GLFW.glfwCreateWindow(640, 480, "LWJGL Bootcamp", NULL, NULL);
+    if(window == NULL) {
+		  throw new IllegalStateException("Unable to create GLFW Window");
+    }
+		
+    GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
+		
+    try(MemoryStack stack = stackPush()){
+		  IntBuffer pWidth = stack.mallocInt(1);
+		  IntBuffer pHeight = stack.mallocInt(1);
+		
+		  GLFW.glfwGetWindowSize(window, pWidth, pHeight);
+			
+		  GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+		
+		  GLFW.glfwSetWindowPos(window,(vidmode.width() - pWidth.get(0)) / 2,(vidmode.height() - pHeight.get(0)) / 2);
+			
+		  GLFW.glfwMakeContextCurrent(window);
+		  GLFW.glfwSwapInterval(1);
+		  GLFW.glfwShowWindow(window);
+    }
+  }
+
+  /**
+  *The main game loop
+  */
+  public void loop(){
+    GL.createCapabilities();
+	  
+    float[] vertices = {-0.5f,-0.5f, 0.0f,
+                         0.5f,-0.5f, 0.0f,
+                         0.0f, 0.5f, 0.0f};
+	  
     int[] indices = {0,1,2};
-    
-    Mesh meshmeyek = MeshLoader.createMesh(vertices,indices);
-    
-    while(!Display.isCloseRequested()){
-      GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-      
-      GL30.glBindVertexArray(meshmeyek.getVaoID());
-	    GL20.glEnableVertexAttribArray(0);
-	    GL11.glDrawElements(GL11.GL_TRIANGLES, meshmeyek.getVertexCount(), GL11.GL_UNSIGNED_INT,0);
-    	GL20.glDisableVertexAttribArray(0);
-	    GL30.glBindVertexArray(0);
-      
-	    Display.update();
-	    Display.sync(60);
+
+    Mesh meshmeyek = MeshLoader.createMesh(vertices,indices); //Kudos if you got that reference	  
+
+    while(!GLFW.glfwWindowShouldClose(window)) {
+		  GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
+		  
+	    	  GL30.glBindVertexArray(meshmeyek.getVaoID());
+		  GL20.glEnableVertexAttribArray(0);
+		  GL11.glDrawElements(GL11.GL_TRIANGLES, meshmeyek.getVertexCount(), GL11.GL_UNSIGNED_INT,0);
+		  GL20.glDisableVertexAttribArray(0);
+	          GL30.glBindVertexArray(0);
+	    
+		  GLFW.glfwSwapBuffers(window);
+		  GLFW.glfwPollEvents();
     }
   }
 
   public static void main(String[] args){
-    new Boot();
+    new Boot().run();
   }
 }
